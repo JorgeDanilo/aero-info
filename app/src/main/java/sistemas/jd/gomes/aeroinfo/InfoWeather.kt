4b51e4975c39858c6ed910f6a29e0181a3832a58
Jorge Danilo <sobrejavatec@gmail.com>
2022-06-21 15:26:39 -0300

    first commit

---
+++ app/src/main/java/sistemas/jd/gomes/aeroinfo/InfoWeather.kt
@@ -0,0 +1,49 @@
+package sistemas.jd.gomes.aeroinfo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InfoWeather() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.teal_700))
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Informações Metereologicas",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Informações Metereologicas",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun ShowScreen() {
+    InfoWeather()
+}
---
+++ app/src/main/java/sistemas/jd/gomes/aeroinfo/ui/theme/Color.kt
@@ -0,0 +1,44 @@
+package sistemas.jd.gomes.aeroinfo.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val BlueGrey900 = Color(0xFF263238)
+val Grey100 = Color(0xFFF5F5F5)
+
+val Color2 = Color(0xFF7890A8)
+val Color3 = Color(0XFF304878)
+val Color4 = Color(0XFF181848)
+val Color5 = Color(0XFFF0A818)
+val Color6 = Color(0XFF90ff17)
+
+val Colors.GrayPrimary
+    get() = Color2
+
+val Colors.GrayDark
+    get() = Color3
+
+val Colors.BlueDark
+    get() = Color4
+
+val Colors.YellowDark
+    get() = Color5
+
+val Colors.GreenDark
+    get() = Color6
+
+val Colors.AppThemColor
+    get() = if (isLight) Color.White else Color.Black
+
+val Colors.AppContentColor
+    get() = if (isLight) Color.Black else Color.White
+
+val Colors.TitleColor
+    get() = if (isLight) Color.Black else Color.White
+
+val Colors.ItemBackgroundColor
+    get() = if (isLight) Grey100 else BlueGrey900
