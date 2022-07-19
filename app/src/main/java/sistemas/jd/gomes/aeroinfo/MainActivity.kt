0bd6d68cfa77323d26082689250e6dc59356e108
Jorge Danilo <sobrejavatec@gmail.com>
2022-07-19 19:07:10 -0300

    auto complete.

--- app/src/main/java/sistemas/jd/gomes/aeroinfo/MainActivity.kt
+++ app/src/main/java/sistemas/jd/gomes/aeroinfo/MainActivity.kt
@@ -3,7 +3,13 @@ package sistemas.jd.gomes.aeroinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import sistemas.jd.gomes.aeroinfo.presentation.detail.info.DetailScreen
+import sistemas.jd.gomes.aeroinfo.presentation.detail.info.TextFieldWithDropDown
 import sistemas.jd.gomes.aeroinfo.presentation.detail.info.weather.InfoWeather
 import sistemas.jd.gomes.aeroinfo.presentation.list.ListAirport
 import sistemas.jd.gomes.aeroinfo.ui.theme.AeroinfoTheme
@@ -13,7 +19,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AeroinfoTheme {
-                DetailScreen()
+                Column {
+                    TextFieldWithDropDown(
+                        items = listOf("sbgo", "sbnv", "sbsp", "sbpa"),
+                        selectedValue = "",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) { index ->
+                        println("$index was selected")
+                    }
+                }
             }
         }
     }
--- app/src/main/java/sistemas/jd/gomes/aeroinfo/presentation/detail/info/DetailScreen.kt
+++ app/src/main/java/sistemas/jd/gomes/aeroinfo/presentation/detail/info/DetailScreen.kt
@@ -1,67 +1,54 @@
 package sistemas.jd.gomes.aeroinfo.presentation.detail.info
 
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import sistemas.jd.gomes.aeroinfo.ui.theme.GrayDark
 import sistemas.jd.gomes.aeroinfo.ui.theme.YellowDark
+import sistemas.jd.gomes.aeroinfo.R
 
 @Composable
 fun DetailScreen() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = {
-            BottomNavigation(navController = navController)
-        }
-    ) {
-        NavigationGraph(navController = navController)
-    }
+
 }
 
 @Composable
-fun BottomNavigation(navController: NavController) {
-    val items = listOf(
-        BottomNavItem.INFO_WEATHER,
-        BottomNavItem.INFO_GENERAL,
-        BottomNavItem.INFO_CHARTS,
-        BottomNavItem.INFO_ROTAER
-    )
+fun TextFieldWithDropDown(
+    items: List<String>,
+    selectedValue: String?,
+    modifier: Modifier = Modifier,
+    onSelect: (Int) -> Unit
+) {
+    AndroidView(
+        factory = { context ->
+            val autoCompleteTextView = AutoCompleteTextView
+                .inflate(context, R.layout.text_input_field, null) as AutoCompleteTextView
 
-    BottomNavigation(
-        backgroundColor = MaterialTheme.colors.GrayDark,
-        contentColor = Color.White
-    ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach { item ->
-            BottomNavigationItem(
-                icon = {
-                    Icon(item.icon, contentDescription = item.title)
-                },
-                label = {
-                    Text(text = item.title, fontSize = 9.sp)
-                },
-                selectedContentColor = MaterialTheme.colors.YellowDark,
-                unselectedContentColor = Color.Black.copy(0.4f),
-                alwaysShowLabel = true,
-                selected = currentRoute == item.screeRoute,
-                onClick = {
-                    navController.navigate(item.screeRoute) {
-                        navController.graph.startDestinationRoute?.let { screenRoute ->
-                            popUpTo(screenRoute) {
-                                saveState = true
-                            }
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
+            val adapter =
+                ArrayAdapter(context, android.R.layout.simple_expandable_list_item_1, items)
+            autoCompleteTextView.setAdapter(adapter)
+            autoCompleteTextView.setText(selectedValue, false)
+            autoCompleteTextView.setOnItemClickListener { _, _, index, _ -> onSelect(index) }
+            autoCompleteTextView
+        },
+        update = { autoCompleteTextView ->
+            val adapter = ArrayAdapter(
+                autoCompleteTextView.context,
+                android.R.layout.simple_list_item_1,
+                items
             )
-        }
-    }
+            autoCompleteTextView.setAdapter(adapter)
+            autoCompleteTextView.setText(selectedValue, false)
+        },
+        modifier = modifier
+    )
 }
